7dd0adcdf14192c552de5de9d8e88333c2379d34
alejandro.vicente <alejandro.vicente@eurecat.org>
2024-02-07 05:13:44 -0500

    Initial files from spring boot initializr

---
+++ src/main/java/com/w2m/supermaintenance/SupermaintenanceApplication.java
@@ -0,0 +1,13 @@
+package com.w2m.supermaintenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermaintenanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermaintenanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/w2m/supermaintenance/SupermaintenanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w2m.supermaintenance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermaintenanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
